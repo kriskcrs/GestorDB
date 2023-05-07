8bfdef4675a16c3e04c3e79229dc4c9db8676254
Cristian Cáceres <ccaceresm1@miumg.edu.gt>
2023-05-07 11:07:27 -0600

    prueba de push

--- src/main/java/com/umg/gestordbbackend/GestorDbBackendApplication.java
+++ src/main/java/com/umg/gestordbbackend/GestorDbBackendApplication.java
@@ -2,7 +2,7 @@ package com.umg.gestordbbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Servicio creado por Cristian
 @SpringBootApplication
 public class GestorDbBackendApplication {
 
