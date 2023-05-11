540363be344fd2b6fbba296ec9dd5851a77e6c5d
Cristian Cáceres <ccaceresm1@miumg.edu.gt>
2023-05-10 23:35:46 -0600

    creacion de tablas dinamicas

--- src/main/java/com/umg/gestordbbackend/Service/CustomTableService.java
+++ src/main/java/com/umg/gestordbbackend/Service/CustomTableService.java
@@ -1,7 +1,6 @@
 package com.umg.gestordbbackend.Service;
 
 import com.umg.gestordbbackend.Entity.CustomEntity;
-import com.umg.gestordbbackend.Repository.CustomTableRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
@@ -14,31 +13,36 @@ import java.util.regex.Pattern;
 @RequestMapping("/custom-table")
 public class CustomTableService {
 
-    @Autowired
-    private CustomTableRepository customTableRepository;
+    public boolean cumple = false;
 
-   // @Autowired
-   // private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     //Autor Cristian Cáceres
     @PostMapping
     public String createTable(@RequestBody CustomEntity table) {
-        try {
-
-            String sentencia=table.getSentencia();
-
-          //  String createTableSql = "CREATE TABLE  valor (ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,nombre VARCHAR(50), email VARCHAR(100))";
 
 
-            System.out.println(sentencia);
+        String sentencia = table.getSentencia();
+        String expresion = "create table [a-z0-9]+ \\((([a-z]+ (int|(varchar\\([0-9]+\\)))),?)+\\);";
 
-            //jdbcTemplate.execute(createTableSql);
-            return "0 Exito en la creacion de la tabla";
-        } catch (Exception e) {
-            System.out.println("Causa -> " + e.getCause());
-            System.out.println("Excepcion -> " + e.getMessage());
-            return "1 Hubo un problema, tabla no creada";
+        validarExpresionRegular(sentencia, expresion);
+
+        if (cumple) {
+            try {
+                jdbcTemplate.execute(sentencia);
+                return "0 Exito en la creacion de la tabla";
+            } catch (Exception e) {
+                System.out.println("Causa -> " + e.getCause());
+                System.out.println("Exception -> " + e.getMessage());
+            }
         }
+        return "1 Hubo un problema, tabla no creada";
+    }
+
+    public boolean validarExpresionRegular(String texto, String expresionRegular) {
+        Pattern patron = Pattern.compile(expresionRegular);
+        Matcher matcher = patron.matcher(texto);
+        return cumple = matcher.matches();
     }
 }
-
