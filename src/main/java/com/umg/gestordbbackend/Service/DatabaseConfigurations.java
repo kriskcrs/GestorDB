014f0b10a47a09f88e18c67db7d877dfb57db00e
Cristian Cáceres <ccaceresm1@miumg.edu.gt>
2023-05-29 16:39:13 -0600

    se agrega multiples peticiones al metodo execute

--- src/main/java/com/umg/gestordbbackend/Service/DatabaseConfigurations.java
+++ src/main/java/com/umg/gestordbbackend/Service/DatabaseConfigurations.java
@@ -7,16 +7,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @CrossOrigin
 public class DatabaseConfigurations {
 
-    @PostMapping("/connect-db")
+    @PostMapping("/execute-db")
     public List<Map<String, Object>> connectToDatabase(@RequestBody UserDB userDB) {
         System.out.println("Url ->"+userDB.getUrl());
         System.out.println("Usuario ->"+userDB.getUsername());
@@ -26,8 +23,10 @@ public class DatabaseConfigurations {
         String url = "jdbc:mysql://localhost:3306/" + userDB.getUrl() + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
         String username = userDB.getUsername();
         String password = userDB.getPassword();
-        String sentencia = userDB.getSentencia();
+        //String sentencia = userDB.getSentencia();
         List<Map<String, Object>> response = new ArrayList<>();
+        List<String> sentencias = Arrays.asList(userDB.getSentencia().split(";"));
+
         try {
             //Crea el Administrador del Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -35,31 +34,39 @@ public class DatabaseConfigurations {
             Connection conn = DriverManager.getConnection(url, username, password);
             // usa la conexion para la consulta
             Statement stmt = conn.createStatement();
-            boolean isQuery = stmt.execute(sentencia);
-            if (isQuery) {
-                ResultSet rs = stmt.getResultSet();
-                int columnCount = rs.getMetaData().getColumnCount();
-                while (rs.next()) {
-                    Map<String, Object> row = new HashMap<>();
-                    for (int i = 1; i <= columnCount; i++) {
-                        String columnValue = rs.getString(i);
-                        String columnName = rs.getMetaData().getColumnName(i);
-                        row.put(columnName, columnValue);
+
+            for(String senten : sentencias){
+                if(!senten.trim().isEmpty()){
+                    boolean isQuery = stmt.execute(senten);
+                    if (isQuery) {
+                        ResultSet rs = stmt.getResultSet();
+                        int columnCount = rs.getMetaData().getColumnCount();
+                        while (rs.next()) {
+                            Map<String, Object> row = new HashMap<>();
+                            for (int i = 1; i <= columnCount; i++) {
+                                String columnValue = rs.getString(i);
+                                String columnName = rs.getMetaData().getColumnName(i);
+                                row.put(columnName, columnValue);
+                            }
+                            response.add(row);
+                        }
+                    } else {
+                        int result = stmt.getUpdateCount();
+                        if (result >= 0) {
+                            Map<String, Object> message = new HashMap<>();
+                            message.put("message", userDB.getSentencia());
+                            response.add(message);
+                        } else {
+                            Map<String, Object> message = new HashMap<>();
+                            message.put("message", "La sentencia no se pudo ejecutar.");
+                            response.add(message);
+                        }
                     }
-                    response.add(row);
                 }
-            } else {
-                int result = stmt.getUpdateCount();
-                if (result >= 0) {
-                    Map<String, Object> message = new HashMap<>();
-                    message.put("message", userDB.getSentencia());
-                    response.add(message);
-                } else {
-                    Map<String, Object> message = new HashMap<>();
-                    message.put("message", "La sentencia no se pudo ejecutar.");
-                    response.add(message);
-                }
+
             }
+
+
             // Cierra la conexion
             conn.close();
         } catch (Exception e) {
