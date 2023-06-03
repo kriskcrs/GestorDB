c154b4ee12ed85c3a3357112bb564abb9bd7acaa
Cristian Cáceres <ccaceresm1@miumg.edu.gt>
2023-06-03 00:26:40 -0600

    cambios por pruebas se restaura

--- src/main/java/com/umg/gestordbbackend/Service/DatabaseConfigurations.java
+++ src/main/java/com/umg/gestordbbackend/Service/DatabaseConfigurations.java
@@ -1,6 +1,7 @@
 package com.umg.gestordbbackend.Service;
 
 import com.umg.gestordbbackend.Entity.UserDB;
+import jdk.jshell.EvalException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +20,9 @@ public class DatabaseConfigurations {
     private String password;
     private String url;
     private String local = "localhost";
+    private String Error;
+    private String Message;
+
 
     @PostMapping("/execute-db")
     public List<Map<String, Object>> connectToDatabase(@RequestBody UserDB userDB) {
@@ -26,60 +30,59 @@ public class DatabaseConfigurations {
         printLog(userDB);
         CollectorData(userDB);
         List<Map<String, Object>> response = new ArrayList<>();
-        String[] sentencias = userDB.getSentencia().split(";");
 
-        try {
-            //Crea el Administrador del Driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            // Crea la conexion
-            Connection conn = DriverManager.getConnection(url, username, password);
-            // usa la conexion para la consulta
-            Statement stmt = conn.createStatement();
 
-            for (String senten : sentencias) {
-                if (!senten.trim().isEmpty()) {
-                    boolean isQuery = stmt.execute(senten);
-                    if (isQuery) {
-                        ResultSet rs = stmt.getResultSet();
-                        int columnCount = rs.getMetaData().getColumnCount();
-                        while (rs.next()) {
-                            Map<String, Object> row = new HashMap<>();
-                            for (int i = 1; i <= columnCount; i++) {
-                                String columnValue = rs.getString(i);
-                                String columnName = rs.getMetaData().getColumnName(i);
-                                row.put(columnName, columnValue);
+            String[] sentencias = userDB.getSentencia().split(";");
+
+            try {
+                //Crea el Administrador del Driver
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                // Crea la conexion
+                Connection conn = DriverManager.getConnection(url, username, password);
+                // usa la conexion para la consulta
+                Statement stmt = conn.createStatement();
+                for (String senten : sentencias) {
+                    if (!senten.trim().isEmpty()) {
+                        boolean isQuery = stmt.execute(senten);
+                        if (isQuery) {
+                            ResultSet rs = stmt.getResultSet();
+                            int columnCount = rs.getMetaData().getColumnCount();
+                            while (rs.next()) {
+                                Map<String, Object> row = new HashMap<>();
+                                for (int i = 1; i <= columnCount; i++) {
+                                    String columnValue = rs.getString(i);
+                                    String columnName = rs.getMetaData().getColumnName(i);
+                                    row.put(columnName, columnValue);
+                                }
+                                response.add(row);
                             }
-                            response.add(row);
-                        }
-                    } else {
-                        int result = stmt.getUpdateCount();
-                        if (result >= 0) {
-                            Map<String, Object> message = new HashMap<>();
-                            message.put("Mensaje-> ", senten);
-                            response.add(message);
                         } else {
-                            Map<String, Object> message = new HashMap<>();
-                            message.put("Mensaje->", "La sentencia no se pudo ejecutar.");
-                            response.add(message);
+                            int result = stmt.getUpdateCount();
+                            if (result >= 0) {
+                                Map<String, Object> message = new HashMap<>();
+                                message.put("Mensaje-> ", senten);
+                                response.add(message);
+                            } else {
+                                Map<String, Object> message = new HashMap<>();
+                                message.put("Mensaje->", "La sentencia no se pudo ejecutar.");
+                                response.add(message);
+                            }
                         }
                     }
                 }
-
+                // Cierra la conexion
+                conn.close();
+            } catch (Exception e) {
+                Map<String, Object> error = new HashMap<>();
+                System.out.println("Mensaje ->" + e.getMessage());
+                System.out.println("Causa -> " + e.getCause());
+                error.put("error", "" + e.getMessage());
+                response.add(error);
             }
-
-
-            // Cierra la conexion
-            conn.close();
-        } catch (Exception e) {
-            Map<String, Object> error = new HashMap<>();
-            System.out.println("Mensaje ->" + e.getMessage());
-            System.out.println("Causa -> " + e.getCause());
-            error.put("error", "" + e.getMessage());
-
-            response.add(error);
-        }
         return response;
+
     }
+
 
     @PostMapping("/check-db")
     public Map<String, Object> checkDatabaseAccess(@RequestBody UserDB userDB) {
@@ -91,11 +94,13 @@ public class DatabaseConfigurations {
             Class.forName("com.mysql.cj.jdbc.Driver");
             // Crea la conexion
             Connection conn = DriverManager.getConnection(url, username, password);
-            response.put("Mensaje", "Conexión exitosa a la base de datos.");
+            Message = "Conexión exitosa a la base de datos.";
+            response.put("Mensaje",Message);
             // Cierra la conexion
             conn.close();
         } catch (Exception e) {
-            response.put("error", "Error: " + e.getMessage()+"\n");
+            Error = e.getMessage();
+            response.put("error", "Error: " + Error+"\n");
         }
         return response;
     }
@@ -103,6 +108,8 @@ public class DatabaseConfigurations {
     public void printLog(UserDB userDB) {
         System.out.println("Sentencia -> " + userDB.getSentencia());
         System.out.println("usuario -> " + userDB.getUsername());
+        System.out.println("respuesta -> " + Message);
+        System.out.println("Error -> "+ Error);
         System.out.println("---------------------------------------------");
     }
 
@@ -111,4 +118,6 @@ public class DatabaseConfigurations {
         username = userDB.getUsername();
         password = userDB.getPassword();
     }
+
+
 }
